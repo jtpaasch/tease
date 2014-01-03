7c94e3884d53e0f673b4ccd502827127d056ffae
JT Paasch <jt.paasch@gmail.com>
2014-01-03 13:13:55 -0500

    Adding some autoloading information to composer.json

---
+++ src/Tease/Teaser.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ *  This file is part of the Tease package.
+ *  See the LICENSE distributed with this source code.
+ */
+
+namespace Teaser;
+
+/**
+ *  This class makes http requests and then
+ *  checks to make sure logs are written correctly.
+ *
+ *  @author JT Paasch <jt.paasch@gmail.com>
+ */
+class Teaser {
+
+    /**
+     *  The constructor.
+     *
+     *  @access public
+     *  @return Teaser instance
+     */
+    public function __construct() {
+
+        echo 'A new teaser.';
+
+    }
+
+
+}
